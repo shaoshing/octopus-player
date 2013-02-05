53beb8503099c5fe91352836becfc96c0fdf87fc
Shaoshing <sxlee87@gmail.com>
2013-02-05 22:43:36 +0800

    Show text animation after clicked icon.

--- inject.js
+++ inject.js
@@ -5,8 +5,9 @@ script.onload = function() {
   this.parentNode.removeChild(this);
 };
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-  if (request.method === "play-with-quicktime")
+chrome.extension.onMessage.addListener(function(request, sender, responseCallback) {
+  if (request.method === "play-with-quicktime"){
     window.postMessage("quicktime", '*');
+  }
   return true;
 });
--- main.js
+++ main.js
@@ -1,8 +1,37 @@
+
+
 chrome.browserAction.onClicked.addListener(function(tab){
-  chrome.browserAction.setBadgeText ( { text: "..." } );
-  chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.sendRequest(tab.id, { method: "play-with-quicktime"}, function(response){
-      chrome.browserAction.setBadgeText ( { text: "" } );
-    });
-  });
+  startLoadingAnimation();
+  playWithQuickTime();
 });
+
+
+function playWithQuickTime(){
+  chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.sendMessage(tab.id, { method: "play-with-quicktime"}, function(){});
+  });
+}
+
+
+loadingAnimation = null;
+const textMaxLength = 5;
+const animationTime = 6000;
+const animationSpeed = 400;
+function startLoadingAnimation(){
+  stopLoadingAnimation();
+  var text = "."
+  loadingAnimation = setInterval(function(){
+    if(text.length > textMaxLength)
+      text = ".";
+    chrome.browserAction.setBadgeText ({ text: text });
+    text += ".";
+  }, animationSpeed);
+  setTimeout(stopLoadingAnimation, animationTime);
+}
+function stopLoadingAnimation(){
+  if(loadingAnimation !== null){
+    clearInterval(loadingAnimation)
+    loadingAnimation = null;
+    chrome.browserAction.setBadgeText ({ text: "" });
+  }
+}
